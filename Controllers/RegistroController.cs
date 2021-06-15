6f87598970ef6fa2b3a0f8c5fadccdf046af2f8f
Jonathan Berroa <berroa-jonatan@outlook.es>
2021-06-15 03:54:22 -0500

    ticket

--- Controllers/RegistroController.cs
+++ Controllers/RegistroController.cs
@@ -3,12 +3,14 @@ using System.Linq;
 using MaxService.Models.Cliente;
 using Microsoft.AspNetCore.Mvc;
 using MonoGamingStore.Data;
+using Rotativa.AspNetCore;
 
 namespace MaxService.Controllers
 {
-    public class RegistroController:Controller
+    public class RegistroController : Controller
     {
-        private  readonly ApplicationDbContext  _context;
+        private readonly ApplicationDbContext _context;
+
         public RegistroController(ApplicationDbContext context)
         {
             _context = context;
@@ -17,23 +19,41 @@ namespace MaxService.Controllers
         public IActionResult Documento(int id)
         {
             var doc = _context.DataRegistro.OrderBy(x => x.RegistroId).Where(x => x.RegistroId == id).ToList();
+            ViewData["idd"] = id;
+
             //return new ViewAsPdf("Documento");
+
             return View(doc);
         }
+        public IActionResult Pdf(int id)
+        {
 
-        public IActionResult Registrar(){
+            var doc = _context.DataRegistro.OrderBy(x => x.RegistroId).Where(x => x.RegistroId == id).ToList();
+            return new ViewAsPdf(doc);
+        }
+
+        public IActionResult Registrar()
+        {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Registrar(Registro r){
+        public IActionResult Registrar(Registro r)
+        {
 
-            if(ModelState.IsValid){
+            if (ModelState.IsValid)
+            {
                 _context.Add(r);
                 _context.SaveChanges();
-                return RedirectToAction("Documento",new { id = r.RegistroId});
+                return RedirectToAction("Documento", new { id = r.RegistroId });
             }
             return View(r);
+        }
+
+        public IActionResult Ticket(int id)
+        {
+            var doc = _context.DataRegistro.OrderBy(x => x.RegistroId).Where(x => x.RegistroId == id).ToList();
+            return View(doc);
         }
 
     }
